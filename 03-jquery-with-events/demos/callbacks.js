58de53f03d0ac3e042f052049ed4d3ff70bddd2c
Allie <allie@codefellows.com>
2017-11-29 16:54:58 -0800

    pushing callback demo from repl.it

---
+++ 03-jquery-with-events/demos/callbacks.js
@@ -0,0 +1,14 @@
+// ANONYMOUS CALLBACK
+
+element.addEventListener('click', function() {
+  // do something when the element is clicked
+})
+
+
+// NAMED CALLBACK
+
+element.addEventListener('click', eventHandler);
+
+function eventHandler() {
+  // do something when the element is clicked
+}
