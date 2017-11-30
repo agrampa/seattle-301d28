0161b02a6bc4c3d1ecaec585a45f32afa04f3b2b
Allie <allie@codefellows.com>
2017-11-30 11:52:24 -0800

    adding day 04 demos

---
+++ 04-templating/demos/handlebars/scripts/neighborhood.js
@@ -0,0 +1,27 @@
+'use strict';
+
+let neighborhoods = [];
+
+// REVIEW: This is another way to use a constructor to duplicate an array of raw data objects, and is helpful when you have a lot of keys or want to easily add more properties later.
+function Neighborhood (rawDataObject) {
+  for (let key in rawDataObject) {
+    this[key] = rawDataObject[key];
+  }
+};
+
+Neighborhood.prototype.toHtml = function() {
+  // 1. Get the template from the HTML document
+
+  // 2. Use Handlebars to "compile" the HTML, returns a function
+
+  // 3. Do not forget to return the HTML from this method
+
+};
+
+neighborhoodDataSet.forEach(function(neighborhoodObject) {
+  neighborhoods.push(new Neighborhood(neighborhoodObject));
+});
+
+neighborhoods.forEach(function(ourNewNeighborhoodObject){
+  $('#neighborhoods').append(ourNewNeighborhoodObject.toHtml());
+});
